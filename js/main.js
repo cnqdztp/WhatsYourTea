b6f72d2d8694dd7f8786ca1898a6daf93ae871cc
气泡缓释胶囊 <japanztp@gmail.com>
2022-01-12 18:38:46 +0800

    removing useless file

--- js/main.js
+++ js/main.js
@@ -1,13 +1,50 @@
-var app = {
+var jsFunction = {
+  //function getCookie
+  getCookie: function(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+  },
+  //function setCookie
+  setCookie: function(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+  },
+
+
   start: function () {
-    $('.sBtn').click(function () {
-      $('.mark').show();
-    });
-    $('.djks').click(function () {
-      var name = $('input').val();
-      window.sessionStorage.setItem('name', name);
-      location.href = './answer/answer.html';
-    });
+    //if name exist in cookie
+    if (jsFunction.getCookie("name") != "") {
+      $('.sBtn').click(function () {
+        location.href = './answer/answer.html';
+      });
+    }else {
+      $('.sBtn').click(function () {
+        $('.mark').show();
+      });
+  
+      $('.djks').click(function () {
+        var name = $('input').val();
+        //store name in cookie
+        jsFunction.setCookie("name", name, 1);
+        location.href = './answer/answer.html';
+      });
+    }
+
+
+    
   },
   answer2: function () {
     $('.answerBox ul').on('click', 'li', function () {
@@ -18,7 +55,6 @@ var app = {
       n++;
       console.log(data[n]);
       if (n > 9) {
-        console.log('最后一题');
         location.href = './result.html';
       } else {
         $('.answerBox .answerA').hide();
@@ -37,104 +73,13 @@ var app = {
     });
   },
   result: function () {
-    var name = window.sessionStorage.getItem('name');
-    console.log(name);
+    //get name from cookie
+    var name = jsFunction.getCookie("name");
     $('.reBox h3 span').text(name);
-    var ca = '';
-    html2canvas($(".bg")[0], {
-      onrendered: function (canvas) {
-        //把截取到的图片替换到a标签的路径下载 
-        ca = canvas.toDataURL();
-        $(".save a").attr('href', canvas.toDataURL());
-        //下载下来的图片名字 
-        $(".save a").attr('download', 'share.png');
-        //document.body.appendChild(canvas);
-      }
-      //可以带上宽高截取你所需要的部分内容 
-      //     , 
-      //     width: 300, 
-      //     height: 300 
-    });
-    $('.save').click(function () {
-      $('.mark').show();
-      Window.href=ca;
-    });
-    $('.mark').click(function () {
-      $('.mark').hide();
-    });
-    $('.mark div').click(function(event){
-      event.stopPropagation()
-    });
-    var index = Math.floor((Math.random()*hudata.length)); 
-    console.log(hudata[index]);
-    // console.log(hudata);
-    $('.imgBox img').attr('src','images/'+hudata[index].t+'.png');
-    $('.huname div').text(hudata[index].t);
-    $('.content div').text(hudata[index].r);
-  },
-  answer: function () {
-    var mySwiper = new Swiper('.swiper-container', {
-      direction: 'vertical',
-    lazyLoading : true,
-    mousewheelControl: true,
-    watchSlidesProgress: true,
-    onInit: function(swiper) {
-      swiper.myactive = 0;
-    },
-    onProgress: function(swiper) {
-      for (var i = 0; i < swiper.slides.length; i++) {
-        var slide = swiper.slides[i];
-        var progress = slide.progress;
-        var translate, boxShadow;
 
-        translate = progress * swiper.height * 0.8;
-        scale = 1 - Math.min(Math.abs(progress * 0.2), 1);
-        boxShadowOpacity = 0;
-
-        slide.style.boxShadow = '0px 0px 10px rgba(0,0,0,' + boxShadowOpacity + ')';
-
-        if (i == swiper.myactive) {
-          es = slide.style;
-          es.webkitTransform = es.MsTransform = es.msTransform = es.MozTransform = es.OTransform = es.transform = 'translate3d(0,' + (translate) + 'px,0) scale(' + scale + ')';
-          es.zIndex=0;
-
-
-        }else{
-          es = slide.style;
-          es.webkitTransform = es.MsTransform = es.msTransform = es.MozTransform = es.OTransform = es.transform ='';
-          es.zIndex=1;
-          
-        }
-
-      }
-
-    },
-
-
-    onTransitionEnd: function(swiper, speed) {
-      for (var i = 0; i < swiper.slides.length; i++) {
-      //	es = swiper.slides[i].style;
-      //	es.webkitTransform = es.MsTransform = es.msTransform = es.MozTransform = es.OTransform = es.transform = '';
-
-      //	swiper.slides[i].style.zIndex = Math.abs(swiper.slides[i].progress);
-
-        
-      }
-
-      swiper.myactive = swiper.activeIndex;
-
-    },
-    onSetTransition: function(swiper, speed) {
-
-      for (var i = 0; i < swiper.slides.length; i++) {
-        //if (i == swiper.myactive) {
-
-          es = swiper.slides[i].style;
-          es.webkitTransitionDuration = es.MsTransitionDuration = es.msTransitionDuration = es.MozTransitionDuration = es.OTransitionDuration = es.transitionDuration = speed + 'ms';
-        //}
-      }
-
-    }
-})
+    var index = Math.floor((Math.random()*teaData.length)); 
+    $('.imgBox img').attr('src','images/'+teaData[index].t+'.png');
+    $('.tea-name div').text(teaData[index].t);
+    $('.content div').text(teaData[index].r);
   }
 }
